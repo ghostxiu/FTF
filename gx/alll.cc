48f9305226a536252e39bdef7647979df3cbdc33
Xiu MuRong <ghostxiu@vip.qq.com>
2016-12-05 10:33:50 +0800

    Add files via upload

---
+++ gx/alll.cc
@@ -0,0 +1,9 @@
+#pragma once
+//避免物理上的一个文件被编译多次
+#include "binary_search"
+#include "arr_abort"
+#include "merge_sort"
+#include "mix_arr"
+#include "quick_sort"
+#include "full_permutation"
+#include "create_arr"
